a6e8052f21a93b05e70d5691a5a65ae8112f5abb
vakrilov <alexander.vakrilov@telerik.com>
2015-12-12 15:24:44 +0200

    Initial commit

---
+++ app/main-page.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+import {TextView} from 'ui/text-view';
+import {topmost} from 'ui/frame';
+import {nativeScriptBootstrap} from 'nativescript-angular/application';
+import {Component} from 'angular2/angular2';
+
+
+@Component({
+    selector: 'main',
+    template: `
+<StackLayout>
+    <Label text="The demo goes here" />    
+</StackLayout>
+`,
+})
+class MainPage {
+}
+
+export function pageLoaded(args) {
+    var page = args.object;
+    page.bindingContext = "";
+
+    console.log('BOOTSTRAPPING...');
+    nativeScriptBootstrap(MainPage, []).then((appRef) => {
+        console.log('ANGULAR BOOTSTRAP DONE.');
+    }, (err) => {
+        console.log('ERROR BOOTSTRAPPING ANGULAR');
+        let errorMessage = err.message + "\n\n" + err.stack;
+        console.log(errorMessage);
+
+        let view = new TextView();
+        view.text = errorMessage;
+        topmost().currentPage.content = view;
+    });
+
+}
