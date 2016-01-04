f3df517a734adda6b70dc71ad825d721465e7a53
Hristo Deshev <hristo@deshev.com>
2016-01-04 11:15:35 +0200

    Play with the router.navigate(...) API.

--- app/group/group.ts
+++ app/group/group.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES, RouteParams } from 'angular2/router';
+import {ROUTER_DIRECTIVES, RouteParams, Router } from 'angular2/router';
 import {groups, ExampleGroup} from "../model/examples-model";
 import {ExamplesListComponenet} from "../examples-list/examples-list";
 import {ListSettingsService} from "../components/list-settings-service";
@@ -12,7 +12,8 @@ import {ListSettingsService} from "../components/list-settings-service";
 })
 export class GroupComponenet {
     public group: ExampleGroup;
-    constructor(params: RouteParams) {
+    constructor(params: RouteParams, private router: Router) {
+        console.log('ROUTER: ' + (<any>this.router.constructor).name);
         var id = params.get("id");
         console.log("GROUP id: " + id);
         for (var group of groups) {
@@ -21,5 +22,10 @@ export class GroupComponenet {
                 break;
             }
         }
+    }
+
+    public routerNavigate() {
+        console.log("Navigating to main...");
+        this.router.navigate(["Main"]);
     }
 }
